16b63f625f638f9f892631d6d82d3d06c5d8ab38
shalom-sims <shalom.sims@camsoftwarellc.com>
2025-05-29 16:05:03 -0500

    Initial Commit

---
+++ InfiniteCardsTests/InfiniteCardsTests.swift
@@ -0,0 +1,17 @@
+//
+//  InfiniteCardsTests.swift
+//  InfiniteCardsTests
+//
+//  Created by Shalom Sims on 5/29/25.
+//
+
+import Testing
+@testable import InfiniteCards
+
+struct InfiniteCardsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
